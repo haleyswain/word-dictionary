c36081969feca09e9677cfc78d68d4fe2c6147d2
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2016-12-06 23:00:39 -0800

    add passing specs and methods for definition class

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,14 +1,39 @@
 class Definition
+  attr_reader(:definition_1)
   @@meanings = []
-
-  attr_reader(:definition, :word_id)
 
   define_method(:initialize) do |attributes|
     @definition = attributes.fetch(:definition)
-    @word_id = attributes.fetch(:word_id)
+    @word_id = @@meanings.length().+(1)
+  end
+
+  define_method(:definition) do
+    @definition
+  end
+
+  define_method(:save) do
+    @@meanings.push(self)
   end
 
   define_singleton_method(:all) do
     @@meanings
+  end
+
+  define_singleton_method(:clear) do
+    @@meanings = []
+  end
+
+  define_method(:word_id) do
+    @word_id
+  end
+
+  define_singleton_method(:find) do |id|
+    found_definition = nil
+    @@meanings.each do |answer|
+      if answer.word_id() == id
+        found_definition = answer
+      end
+    end
+    found_definition
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,10 +2,50 @@ require('rspec')
 require('definition')
 
 
-describe (Definition) do
+describe ('Definition') do
+  before() do
+    Definition.clear()
+    @test_definition = Definition.new(:definition => "it is a plant")
+    @test_definition.save()
+  end
+
+describe(".clear") do
+   it("empties the array of saved definitions") do
+     Definition.clear()
+     expect(Definition.all()).to(eq([]))
+   end
+ end
+
+  describe('#definition') do
+    it('returns the definition of the word') do
+      expect(@test_definition.definition()).to(eq('it is a plant'))
+    end
+  end
+
   describe(".all") do
-    it("is empty at first") do
-      expect(Definition.all()).to(eq([]))
+    it("returns all saved definitions") do
+      expect(Definition.all()).to(eq([@test_definition]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a word to the array of saved words") do
+      test_word = Word.new("it is a plant")
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe(".find") do
+    it("returns the definitions id") do
+      expect(Definition.find(@test_definition.word_id())).to(eq(@test_definition))
+    end
+  end
+
+
+  describe("#word_id") do
+    it("returns the id of the definition") do
+      expect(@test_definition.word_id()).to(eq(1))
     end
   end
 end
